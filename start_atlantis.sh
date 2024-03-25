5d57c6ca1c68fc4e1e439c49668a4edce51a392f
Andrade,Miqueas <miqueas.andrade@effem.com>
2024-03-25 10:53:38 -0300

    initial Atlantis Configuration commit

---
+++ start_atlantis.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+# Define the Atlantis project folder
+ATLANTIS_FOLDER="/home/andramiq/Workspaces/az2-atlantis-poc"
+# Define the Atlantis executable location
+ATLANTIS_PATH="/usr/bin/atlantis"  # Change this to the full path if atlantis is not in your PATH
+# Define the Atlantis configuration file location
+ATLANTIS_CONFIG="${ATLANTIS_FOLDER}/atlantis.yaml"  # Change this to your Atlantis config file path
+
+# Function to start Atlantis
+start_atlantis() {
+    cd ${ATLANTIS_FOLDER}
+    echo "Starting Atlantis server..."
+    nohup $ATLANTIS_PATH server --config $ATLANTIS_CONFIG > atlantis.log 2>&1 &
+    echo $! > atlantis.pid
+    # lsof -i -p $(cat atlantis.pid) | awk '{print $2}' | egrep -v PID &>> atlantis.pid
+    echo "Atlantis started with PID $(cat atlantis.pid)"
+    cat atlantis.log
+}
+
+# Function to stop Atlantis
+stop_atlantis() {
+    cd ${ATLANTIS_FOLDER}
+    if [ ! -f atlantis.pid ]; then
+        echo "Atlantis is not running."
+        return
+    fi
+
+    echo "Stopping Atlantis server..."
+    kill $(cat atlantis.pid | xargs -n100)
+    rm atlantis.pid
+    echo "Atlantis stopped."
+}
+
+# Main logic to start/stop Atlantis based on user input
+case "$1" in
+    start)
+        start_atlantis
+        ;;
+    stop)
+        stop_atlantis
+        ;;
+    *)
+        echo "Usage: $0 {start|stop}"
+        exit 1
+        ;;
+esac
